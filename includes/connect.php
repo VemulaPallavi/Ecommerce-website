b00792a5f6ff1537a0a8131f247677344009d9af
VemulaPallavi <118461080+VemulaPallavi@users.noreply.github.com>
2023-03-28 21:08:03 +0530

    hello

---
+++ includes/connect.php
@@ -0,0 +1,6 @@
+<?php
+$con=mysqli_connect('localhost:3322','root','','mystore');
+if(!$con){
+    die(mysqli_error($con));
+}
+?>
---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecommerce Website</title>
+<!--bootstrap css link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<!--font awesome link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3Pyo1Y1cOd4DSE0Ga+ri$AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
+  <link rel="stylesheet" href="style.css">
+  <style>
+    .admin_image{
+    width:100px;
+    object-fit: contain;
+}
+.footer{
+  position:absolute;
+  buttom:0;
+}
+</style>
+</head>
+<body>
+  <!--navbar-->
+  <div class="container-fluid p-0">
+    <!--first child-->
+  <nav class="navbar navbar-expand-lg navbar-light bg-info">
+  <div class="container-fluid">
+    <img src="./images/logo.png" alt="" class="logo">
+   
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+       
+        <li class="nav-item">
+          <a class="nav-link" href="#">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Total Price:100/-</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<!--second child-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+  <ul class="navbar-nav me-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Welcome Guest</a>
+</li>
+<li class="nav-item">
+      <a class="nav-link" href="#">Login</a>
+</li>
+</ul>
+</nav>
+<!--third child-->
+<div class="bg-light">
+  <h3 class="text-center">Hidden Store</h3>
+  <p class="text-center">Communication is at the heart of e-commerce and community</p>
+</div>
+<!--fourth child-->
+<div class="row">
+  <div class="col-md-10">
+    <!--products-->
+    <div class="row">
+      <div class="col-md-4">
+      <div class="card" >
+      <img src="./images/apple.jpg" class="card-img-top"alt="..."> 
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div>
+      </div>
+      <div class="col-md-4">
+      <div class="card">
+      <img src="./images/capsicum.jpg" class="card-img-top"alt="..."> 
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div>
+      </div> <div class="col-md-4">
+      <div class="card">
+      <img src="./images/capsicum.jpg" class="card-img-top"alt="..."> 
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div>
+      </div>
+       <div class="col-md-4 mb-2">
+      <div class="card">
+      <img src="./images/capsicum.jpg" class="card-img-top"alt="..."> 
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div>
+
+      </div>
+      <div class="col-md-4 mb-2">
+      <div class="card">
+      <img src="./images/capsicum.jpg" class="card-img-top"alt="..."> 
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div>
+      </div>
+      <div class="col-md-4 mb-2">
+        <div class="card" >
+        <img src="./images/dairy.jpg" class="card-img-top"alt="..."> 
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View more</a>
+  </div>
+</div></div>
+</div>
+  </div>
+  <div class="col-md-2 bg-secondary p-0">
+    <!--brands to be displayed-->
+    <ul class="navbar-nav me-auto text-center">
+      <li class="nav-item bg-info">
+        <a href="#" class="nav-link text-light"><h4>Delivery Brands</h4></a>
+      </li>
+      <li class="nav-item ">
+        <a href="#" class="nav-link text-light"><h4>Brand1</h4></a>
+      </li>
+      <li class="nav-item ">
+        <a href="#" class="nav-link text-light"><h4>Brand2</h4></a>
+      </li>
+      <li class="nav-item ">
+        <a href="#" class="nav-link text-light"><h4>Brand3</h4></a>
+      </li>
+      <li class="nav-item ">
+        <a href="#" class="nav-link text-light"><h4>Brand4</h4></a>
+      </li>
+      <li class="nav-item ">
+        <a href="#" class="nav-link text-light"><h4>Brand5</h4></a>
+      </li>
+</ul>
+ 
+<!--catagories to be displayed--> 
+<ul class="navbar-nav me-auto text-center">
+      <li class="nav-item bg-info">
+        <a href="#" class="nav-link text-light"><h4>Categories</h4></a>
+      </li>
+      <li class="nav-item ">
+        <a href="#" class="nav-link text-light"><h4>juices</h4></a>
+      </li>
+      <li class="nav-item ">
+        <a href="#" class="nav-link text-light"><h4>milk products</h4></a>
+      </li>
+      <li class="nav-item ">
+        <a href="#" class="nav-link text-light"><h4>chips</h4></a>
+      </li>
+      <li class="nav-item ">
+        <a href="#" class="nav-link text-light"><h4>Categories4</h4></a>
+      </li>
+      <li class="nav-item ">
+        <a href="#" class="nav-link text-light"><h4></h4></a>
+      </li>
+</ul>
+</div>
+  </div>
+<!--last child-->
+<div class="bg-info p-3 text-center">
+  <p>All rigths reserved <i class="fa fa-copyright" aria-hidden="true"></i>- Designed by Vemula-2023</p>
+</div>
+</div>
+<!--bootstrap js link-->
+<script src="http://cdn.jsdeliver.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"integrity="sha384-ka750G1n4gmtzMlQnikT1wXgYs0g+0MhuP+I1RH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anoymous"></script>
+</body>
+</html>
